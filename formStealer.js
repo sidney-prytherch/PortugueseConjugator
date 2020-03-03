ac6bf12c04103b0236d2e048a04beb5e7b0c6407
sidney-prytherch <sidneyprytherch@gmail.com>
2020-03-03 17:11:56 -0500

    Update formStealer.js

--- formStealer.js
+++ formStealer.js
@@ -17,3 +17,51 @@ let process = (prefix, takeaway, str) => {
     x.setSelectionRange(0, 99999);
     document.execCommand("copy");
 }
+
+
+let table = `Infinitive
+Impersonal	fazer
+Personal	fazer	fazeres	fazer	fazermos	fazerdes	fazerem
+Gerund
+fazendo
+Past participle
+Masculine	feito	feitos
+Feminine	feita	feitas
+Indicative
+Present	faço	fazes	faz	fazemos	fazeis	fazem
+Imperfect	fazia	fazias	fazia	fazíamos	fazíeis	faziam
+Preterite	fiz	fizeste	fez	fizemos	fizestes	fizeram
+Pluperfect	fizera	fizeras	fizera	fizéramos	fizéreis	fizeram
+Future	farei	farás	fará	faremos	fareis	farão
+Conditional
+faria	farias	faria	faríamos	faríeis	fariam
+Subjunctive
+Present	faça	faças	faça	façamos	façais	façam
+Imperfect	fizesse	fizesses	fizesse	fizéssemos	fizésseis	fizessem
+Future	couber	couberes	couber	coubermos	couberdes	couberem
+Imperative
+Affirmative	-	cabe	caiba	caibamos	cabei	caibam
+Negative (não)	-	caibas	caiba	caibamos	caibais	caibam`;
+
+let infinitive = table.match(/(?<=Impersonal\t)(\S*)/g)[0].trim();
+let persInfs = table.match(/(?<=Personal\t)([\S\t]*)/g)[0].trim().split("\t");
+let gerund = table.match(/(?<=Gerund\n)(\S*)/g)[0].trim();
+let participle = table.match(/(?<=Past participle\nMasculine\t)(\S*)/g)[0].trim();
+
+let preses = table.match(/(?<=Present\t)(.*)/g);
+let imperfs = table.match(/(?<=Imperfect\t)(.*)/g);
+let futs = table.match(/(?<=Future\t)(.*)/g);
+
+let presInd = preses[0].trim().split("\t");
+let impInd = imperfs[0].trim().split("\t");
+let pretInd = table.match(/(?<=Preterite\t)(.*)/g)[0].trim().split("\t");
+let plupInd = table.match(/(?<=Pluperfect\t)(.*)/g)[0].trim().split("\t");
+let futInd = futs[0].trim().split("\t");
+let cond = table.match(/(?<=Conditional\n)(.*)/g)[0].trim().split("\t");
+
+let presSubj = preses[1].trim().split("\t");
+let impSubj = imperfs[1].trim().split("\t");
+let futSubj = futs[1].trim().split("\t");
+
+let impAff = table.match(/(?<=Affirmative\t)(.*)/g)[0].trim().split("\t")
+let impAff = table.match(/(?<=Negative \(não\)\t)(.*)/g)[0].trim().split("\t")
